02eb824fe9d7b6016d648bc011391cc95914d872
felixs-alt <114471609+felixs-alt@users.noreply.github.com>
2022-10-13 13:58:39 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
 window.onload = function(){
-document.cookie.mac = NaN;
-x = document.cookie.mac;
+    document.cookie.mac = NaN;
+    x = document.cookie.mac;
     if (x = "04:56:E5:3E:D2:80") {
         alert("Get Trolled");
         window.location = "./trolled.html";
 
-  }
 }
-document.getElementById("deez").onclick = function() {SaveMac()};
+}
 function SaveMac()  {
     macadd = document.getElementById("pd");
     document.cookie = "mac="+macadd.value
     location.reload
 }
+document.getElementById("deez").onclick = function() {SaveMac()};
